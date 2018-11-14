ca092f94ed0f114dc1b0aa2785451fb2b24564ac
TimmyOh <derrickoh19@gmail.com>
2018-11-14 14:09:23 +0800

    update

--- Movie.js
+++ Movie.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = 'mongodb://paul:abc123@ds221242.mlab.com:21242/movies';
+const db = 'mongodb://tim:dotaking007@ds241133.mlab.com:41133/timmovies';
 
 mongoose
   .connect(
